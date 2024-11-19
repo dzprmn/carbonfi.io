9475ede764535ea9e24131986d2ab28710cffe14
Diaz Permana <051511515@ecampus.ut.ac.id>
2024-11-19 10:16:02 +0700

    initial commit

--- vite.config.ts
+++ vite.config.ts
@@ -1,12 +1,24 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-import path from 'path'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
+  define: {
+    'process.env': {},
+    global: 'globalThis',
+  },
+  optimizeDeps: {
+    esbuildOptions: {
+      define: {
+        global: 'globalThis'
+      }
+    }
+  },
   resolve: {
     alias: {
-      '@': path.resolve(__dirname, './src'),
-    },
-  },
-})
+      process: "process/browser",
+      stream: "stream-browserify",
+      util: "util"
+    }
+  }
+});
